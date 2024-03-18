28613e005bfd59f54f97c8f5ca9d9ad2086c25a0
nerodrakar <luizhenriquerezio@gmail.com>
2024-03-18 16:49:50 -0300

    terceira atualização

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ sigma_b = [random.uniform(10000, 15000) for _ in range(10)]
 sigma_t = [random.uniform(10000, 15000) for _ in range(10)]
 x = [1.98, 3.96, 5.94, 7.92, 9.90, 11.80, 13.86, 15.84, 17.82, 19.8]
 
-chart_data1 = pd.DataFrame(({'m': x, 'sigma b': sigma_b}))
-chart_data2 = pd.DataFrame(({'m': x, 'sigma b': sigma_t}))
-st.line_chart(chart_data1)
-st.line_chart(chart_data2)
+chart_data = pd.DataFrame({'sigma t': sigma_t,'sigma b': sigma_b, 'm': x})
+
+st.title("GRAFICO")
+st.line_chart(chart_data, x="m")
