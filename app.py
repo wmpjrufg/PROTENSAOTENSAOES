4fc2177658c1ff32c09a4103134a524013bd0a76
nerodrakar <luizhenriquerezio@gmail.com>
2024-03-18 16:12:54 -0300

    segunda modificação

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 import streamlit as st 
 import pandas as pd 
 import numpy as np
+import random
 
-st.title("mascaico")
+sigma_b = [random.uniform(10000, 15000) for _ in range(10)]
+sigma_t = [random.uniform(10000, 15000) for _ in range(10)]
+x = [1.98, 3.96, 5.94, 7.92, 9.90, 11.80, 13.86, 15.84, 17.82, 19.8]
+
+chart_data1 = pd.DataFrame(({'m': x, 'sigma b': sigma_b}))
+chart_data2 = pd.DataFrame(({'m': x, 'sigma b': sigma_t}))
+st.line_chart(chart_data1)
+st.line_chart(chart_data2)
