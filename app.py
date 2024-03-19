20261db16339ace392183554e9b27d25e79df438
nerodrakar <luizhenriquerezio@gmail.com>
2024-03-19 15:29:56 -0300

    quinta atualização

--- app.py
+++ app.py
@@ -5,19 +5,33 @@ import numpy as np
 
 from protendido import *
 
+# Chamando a função e atribuindo os valores retornados a variáveis
+x, e_p, m_gpp, m_gex, m_q, p_i = carregando_dados()
+# x = [1.98, 3.96, 5.94, 7.92, 9.90, 11.80, 13.86, 15.84, 17.82, 19.8] # vem do usuário
+# e_p = [0.38] * 10 # vem do usuário
+# m_gpp = [1] * 10 # vem do usuário
+# m_gex = [1] * 10 # vem do usuário
+# m_q = [1] * 10 # vem do usuário
+# p_i = [1] * 10 # vem do usuário
 
-x = [1.98, 3.96, 5.94, 7.92, 9.90, 11.80, 13.86, 15.84, 17.82, 19.8] # vem do usuário
-e_p = [0.38] * 10 # vem do usuário
-m_gpp = [] # vem do usuário
-m_gex = [] # vem do usuário
-m_q = [] # vem do usuário
-p_i = [] # vem do usuário
+# Lista de tuplas contendo o nome da variável e seu valor padrão
+variaveis = [("a_c (m2):", 1.00), ("i_c (m4):", 1.00), ("w_t (m3):", 1.00), ("w_b (m3):", 1.00)]
 
-# Pedindo para o usuário inserir elementos
-a_c = st.number_input("a_c (m2):")
-i_c = st.number_input("i_c (m4):")
-w_t = st.number_input("w_t (m3):")
-w_b = st.number_input("w_b (m3):")
+# Dicionário para armazenar os valores das variáveis
+valores = {}
+
+# Pedindo ao usuário para inserir os valores correspondentes
+for nome, valor_padrao in variaveis:
+    valor_digitado = st.number_input(nome, value=valor_padrao)
+    if valor_digitado != "":
+        valores[nome[:-1]] = valor_digitado  # Atualiza o valor no dicionário apenas se o usuário digitou algo
+
+# Atribuindo os valores das variáveis
+a_c = valores.get("a_c (m2)", 1)
+i_c = valores.get("i_c (m4)", 1)
+w_t = valores.get("w_t (m3)", 1)
+w_b = valores.get("w_b (m3)", 1)
+
 
 # Determinando as tensões no Estado Vazio
 sigma_b_t0 = []
@@ -28,6 +42,7 @@ for id, ep_x in enumerate(e_p):
     sigma_pi_b, sigma_pi_t = tensao_protensao(a_c, w_t, w_b, ep_x, 1, p_i[id])
     sigma_b_t0.append(sigma_mg_b + sigma_mq_b + sigma_pi_b)
     sigma_t_t0.append(sigma_mg_t + sigma_mq_t + sigma_pi_t)
+
 
 # Determinando as tensões no Estado Limite de Serviço
 sigma_b_tinf = []
@@ -41,7 +56,18 @@ for id, ep_x in enumerate(e_p):
 
 
 #DATAFRAME 
-chart_data = pd.DataFrame({'sigma t': sigma_t,'sigma b': sigma_b, 'm': x})
+chart_data = pd.DataFrame({
+                            'sigma t t0': sigma_t_t0,
+                            'sigma b t0': sigma_b_t0, 
+                            'sigma t tinf': sigma_t_tinf,
+                            'sigma b tinf': sigma_b_tinf, 
+                            'm': x, 
+                            'p_i': p_i,
+                            'm_gpp': m_gpp,
+                            'm_gex': m_gex,
+                            'm_q': m_q,
+                            })
+chart_data
 
 #CARREGANDO O ARQUIVO EXEL
 def load_data(nrows):
@@ -60,13 +86,20 @@ data_load_state.text('Loading data...done!')
 st.subheader('Base de Dados Inicial')
 st.write(data)
 
+# Chamando a função e atribuindo os valores retornados a variáveis
+x, e_p, m_gpp, m_gex, m_q, p_i = carregando_dados()
 
 
-
-
-st.title("GRAFICOS")
+st.title("GRAFICO 1")
 st.subheader("Topo")
-st.line_chart(chart_data.set_index('m')['sigma t'])
+st.line_chart(chart_data.set_index('m')['sigma t t0'])
 
 st.subheader("Baixo")
-st.line_chart(chart_data.set_index('m')['sigma b'])
+st.line_chart(chart_data.set_index('m')['sigma b t0'])
+
+st.title("GRAFICO 2")
+st.subheader("Topo")
+st.line_chart(chart_data.set_index('m')['sigma t tinf'])
+
+st.subheader("Baixo")
+st.line_chart(chart_data.set_index('m')['sigma b tinf'])
